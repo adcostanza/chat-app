16a5d455eebfeb2396df294968e05a59905926ae
Adam Costanza <acostanza@averoinc.com>
2019-05-23 08:13:44 -0600

    iam roles for rds

--- backend/offline.sh
+++
@@ -1,2 +0,0 @@
-docker-compose up -d
-sls offline --port 9999 
--- backend/src/handlers/messages.ts
+++ backend/src/handlers/messages.ts
@@ -1,11 +1,9 @@
 import { Callback, Context, Handler } from 'aws-lambda';
 import { postgres } from '../db/postgres';
 import { Message } from '../model/model';
-import { ENV } from '../env';
 
 const get: Handler = async (event: any, context: Context, callback: Callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  console.log(ENV);
   await postgres.connect();
   const queryRunner = await postgres.getQueryRunner();
   const messages = (await queryRunner.query('SELECT * from messages')) as Message[];
@@ -19,3 +17,4 @@ const get: Handler = async (event: any, context: Context, callback: Callback) =>
 };
 
 export { get };
+
